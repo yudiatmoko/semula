58791cf26bf838991feee2be08c88d817d485940
Andikasyptr <muhammadandikaanjassyaputra@gmail.com>
2025-12-13 22:52:53 +0700

    logo dan dashboard

---
+++ resources/views/filament/widgets/semula-info.blade.php
@@ -0,0 +1,16 @@
+<x-filament::widget class="h-full">
+    <x-filament::card class="h-full">
+        <div class="flex justify-between items-center h-full">
+            <div>
+                <h2 class="text-base font-semibold tracking-tight">
+                    SEMULA
+                </h2>
+                <p class="text-sm text-gray-500">
+                    Sistem Monitoring Suara Lurah
+                </p>
+            </div>
+
+            <span class="text-xs text-gray-400">v1.0</span>
+        </div>
+    </x-filament::card>
+</x-filament::widget>
