edb2a77173bd0e92eec592042f326ed0077af86e
yudiatmoko <yudiatmokoilham@gmail.com>
2026-02-10 23:07:27 +0700

    redirect root to /admin

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleAuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 
